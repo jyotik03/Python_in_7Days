c1dd4b4e108b1bd911153da214f4ca63a37fe258
jyotik03 <jyotik03322@gmail.com>
2024-10-06 20:35:08 +0530

    Day2

---
+++ Day2/Lab008.py
@@ -0,0 +1,12 @@
+# Identifiers
+
+age = 22  # can start with alphabet
+
+_age = 22  #can start with underscore
+
+age22 = 22
+
+"""invalid representation --> 1. Should not contain special character--> age$123 = 22
+                              2. should not start with number        --> 23age = 22 
+                              3. cannot be reserved word             --> as = 22 """
+
